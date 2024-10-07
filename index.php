427074c3cb80f8d307b06def076cdea47e19b409
0XMatriX <113830943+matrixo-pog@users.noreply.github.com>
2024-10-07 15:20:19 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -27,8 +27,6 @@
 
             $data = json_decode($exec_result, true);
 
-
-            
             $name = $data["location"]["name"];
             $temperature = $data["current"]["temp_c"];
             $wind_mph = $data["current"]["wind_mph"];
@@ -38,7 +36,6 @@
             $pressure_mb = $data["current"]["pressure_mb"];
             $text = $data["current"]["condition"]["text"];
             $icon = $data["current"]["condition"]["icon"];
-
 
             curl_close($ch);
             return [
@@ -65,7 +62,7 @@
 
         //to get $result u should call get_weather($loc) where $loc is string of location in ENGLISH
         //without: "ą, ę, ł, ó, ż, ź" if city is polish
-        //"Los_Angles" => "Los Angeles"
+        //change input: "Los_Angles" => "Los Angeles"
 
     ?>
         <div id="main_block">
@@ -73,29 +70,30 @@
                 <div id="img_info">
                     <?php
                         echo "<img id='weather-icon' src='" . $result["icon"] . "' alt='icon' >";
-                        echo "<h1>" . $result["text"] . "</h1>";
+                        echo "<h1 id='text'>" . $result["text"] . "</h1>";
                         echo "<h1>" . $result["temperature"] . "℃</h1>";
-                        echo "<h2>feels like:" . $result["feelslike_c"] . "℃</h2>";
-                        echo "<h2>wind speed:" . $result["wind_mph"] . "mph</h2>";
-                        echo "<h2>humidity:" . $result["humidity"] . "</h2>";
-                        echo "<h2>clouds:" . $result["cloud"] . "</h2>";
-                        echo "<h2>pressure:" . $result["pressure_mb"] . "</h2>";
+                        echo "<h2>Feels like: " . $result["feelslike_c"] . "℃</h2>";
+                        echo "<h2>Wind speed: " . $result["wind_mph"] . "mph</h2>";
+                        echo "<h2>Humidity: " . $result["humidity"] . "</h2>";
+                        echo "<h2>Clouds: " . $result["cloud"] . "</h2>";
+                        echo "<h2>Pressure: " . $result["pressure_mb"] . "</h2>";
                     ?>
                 </div>
                 <div id="temp_info">
                     <?php
-                        echo "<h1>" . $result["name"] . "</h1>";
+                        echo "<h1 id='city_name'>" . $result["name"] . "</h1>";
                     ?>
                 </div>
             </div>
             <div id="right_info"> 
                 <form action="index.php" method="POST">
                     <!-- ❓🌤️🌧️🌦️⛈️⛅🌥️🌨️🌩️📍🔍🔎 -->
-                    <input id="search" type="text" name="location" placeholder="Search here">
-                    <input id="submit" name='look' type="submit" value="🔎">
+                    <input id="search" type="text" name="location" placeholder="Search here" required>
+                    <button id="submit" name="look" type="submit">🔎</button>
                 </form>
                 <h2>Ostatnie wyszukiwane:</h2>
             </div>
         </div>
     </body>
+    <script src="script.js"></script>
 </html>
