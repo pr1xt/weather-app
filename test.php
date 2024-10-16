375dc89746ead73a2b3ea5e435949dccf17aa02e
0XMatriX <113830943+matrixo-pog@users.noreply.github.com>
2024-10-16 10:20:50 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">  
+<head> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/icon.jpg" type="image/jpg">
+    <link rel="stylesheet" href="style.css">
+    <title>Weather app</title>
+</head>
+<body>
+
+<div id="cloud-container">
+            <div id="cloud1" class="cloud cloud-left"><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffreepngimg.com%2Fthumb%2Fcloud%2F4-cloud-png-image.png&f=1&nofb=1&ipt=a0e8c82c66721d7f0389f402bd3a5625ae2587cb08a5dcc1dd938c1cc4693797&ipo=images"></div>
+            <div id="cloud2"class="cloud cloud-right"><img src="images\cloud_right.png"></div>
+        </div>
+
+</body>
+<script src="script.js"></script>
+</html>
