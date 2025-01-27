28059ba543650bedea2c6ed43cfcc0a68560aa50
pr1xt <timurkn2006@gmail.com>
2025-01-27 22:50:31 +0100

    minor

--- index.php
+++ index.php
@@ -124,36 +124,6 @@
                 $result = get_weather(loc: $location);    
             }
         }
-        //else{
-        //    if(isset($_POST["ChildForm0"])){
-        //        $location_1 = $_POST["loc_0"];
-        //
-        //        $result = get_weather(loc: $location_1);
-        //    }
-        //    if(isset($_POST["ChildForm1"])){
-        //        $location_2 = $_POST["loc_1"];
-        //
-        //        $result = get_weather(loc: $location_2);
-        //    }
-        //    if(isset($_POST["ChildForm2"])){
-        //        $location_3 = $_POST["loc_2"];
-        //
-        //        $result = get_weather(loc: $location_3);
-        //    }
-        //    if(isset($_POST["ChildForm3"])){
-        //        $location_4 = $_POST["loc_3"];
-        //        
-        //        $result = get_weather(loc: $location_4);
-        //    }
-        //    if(isset($_POST["ChildForm4"])){
-        //        $location_5 = $_POST["loc_4"];
-        //        
-        //        $result = get_weather(loc: $location_5);
-        //    }
-        //    else{
-        //        $result = get_weather(loc: "Ohio");
-        //    }
-        //}
 
         function generateRandomNumber($length = 11) {
             $min = pow(10, $length - 1); // Smallest 11-digit number
